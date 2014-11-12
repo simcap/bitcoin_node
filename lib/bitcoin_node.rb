92fd5669122adee1c026b5757cb1616ecac6e96b
simcap <simcap@fastmail.com>
2014-11-12 15:12:19 +0100

    Spaces

--- lib/bitcoin_node.rb
+++ lib/bitcoin_node.rb
@@ -1,15 +1,11 @@
 # coding: ascii-8bit
-
 require 'logger'
 
 require 'bitcoin_node/version'
-
 require 'bitcoin_node/protocol'
 require 'bitcoin_node/p2p'
 
-
 module BitcoinNode
-
   NETWORKS = { main: "\xF9\xBE\xB4\xD9".freeze, testnet: "\xFA\xBF\xB5\xDA".freeze }
 
   def self.network
@@ -18,7 +14,6 @@ module BitcoinNode
 
   Logger = ::Logger.new(STDOUT)
   Logger.progname = 'NODE'
-
 end
 
 BN = BitcoinNode
